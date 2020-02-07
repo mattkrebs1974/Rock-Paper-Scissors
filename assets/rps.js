3cf965a3cd72de13e77ac842e580647ca3a5462d
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-02-07 14:27:09 -0500

    Set in Motion funcion

---
+++ assets/rps.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    var pos=2;
+
+function setInMotion(){
+    
+    setInterval(frame, 100);
+    //1000 means seconds. 
+
+    function frame() {
+        if (pos == 0) {
+            $("#outcomeOne").attr("src", "./Rock.png");
+            $("#outcomeTwo").attr("src", "./Paper.png");
+            pos = 3;
+            pos--;
+           
+        }
+        else if (pos == 1) {
+            
+            $("#outcomeOne").attr("src", "./Paper.png");
+            $("#outcomeTwo").attr("src", "./Scissors.png");
+            pos--; }
+             
+        else if (pos == 2) {
+
+            $("#outcomeOne").attr("src", "./Scissors.png");
+            $("#outcomeTwo").attr("src", "./Rock.png");
+            pos--;
+
+        } else {}
+
+    }
+
+    frame();  
+
+    }
+    setInMotion();
+
+    });
